2ae941cfe96b55bab86a9299755e1a9be9b59259
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-11 17:28:46 +0900

    Add files via upload

---
+++ myproject2/urls.py
@@ -0,0 +1,16 @@
+# myproject2/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    # トップページ → dashboard.urls
+    path('', include('dashboard.urls')),
+    path('schedule/', include('schedule.urls')),
+    path('prompt/', include('prompt.urls')),
+    path('earning/', include('earning.urls')),
+    path('target/', include('target.urls')),
+    path('control/', include('control.urls')),
+    path('trending/', include('trending.urls')),
+]
