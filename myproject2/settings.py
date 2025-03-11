f92b2027ab1d3799ba99eaee0a21ec34dbe80809
yoshi-nakane0 <yos242012@yahoo.co.jp>
2025-03-11 22:00:50 +0900

    Update settings.py

--- myproject2/settings.py
+++ myproject2/settings.py
@@ -3,12 +3,12 @@ from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent().parent
+BASE_DIR = Path(__file__).resolve().parent.parent  # ここを修正
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# --- SECRET_KEY 設定 (ここが重要) ---
+# --- SECRET_KEY 設定 ---
 try:
     SECRET_KEY = os.environ["SECRET_KEY"]
 except KeyError:
